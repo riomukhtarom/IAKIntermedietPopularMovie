7b1c11c6bb87f8902ef3cb5d8da460a0f5339704
Rio Mukhtarom <riomukhtarom10@gmail.com>
2017-12-01 16:43:56 +0800

    initial commit

---
+++ app/src/main/java/com/example/android/iakintermedietpopularmovie/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.android.iakintermedietpopularmovie;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rv_movie);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+}
