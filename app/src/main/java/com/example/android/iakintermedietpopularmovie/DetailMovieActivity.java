574ca81477a4636dfcf77e74360b1e3d4fe7e6a6
Rio Mukhtarom <riomukhtarom10@gmail.com>
2017-12-10 19:47:38 +0800

    bisami detail

--- app/src/main/java/com/example/android/iakintermedietpopularmovie/DetailMovieActivity.java
+++ app/src/main/java/com/example/android/iakintermedietpopularmovie/DetailMovieActivity.java
@@ -1,13 +1,96 @@
 package com.example.android.iakintermedietpopularmovie;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.example.android.iakintermedietpopularmovie.data.MovieContract;
+import com.example.android.iakintermedietpopularmovie.model.Movie;
+import com.example.android.iakintermedietpopularmovie.utils.NetworkUtils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.net.URL;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailMovieActivity extends AppCompatActivity {
+
+    TextView mMovieTitle;
+    ImageView mMovieImage;
+    TextView mMovieYear;
+    TextView mMovieRating;
+
+    //TextView mMovieFavoriteBotton;
+
+    public static final String MOVIE_INTENT = "movie_intent";
+
+    private Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_movie);
+
+        mMovieTitle = (TextView) findViewById(R.id.movie_title);
+        mMovieImage = (ImageView) findViewById(R.id.movie_image);
+        mMovieYear = (TextView) findViewById(R.id.movie_year);
+        mMovieRating = (TextView) findViewById(R.id.movie_rating);
+
+
+        movie = getIntent().getParcelableExtra(MOVIE_INTENT);
+
+        //title
+        mMovieTitle.setText(movie.getTitle());
+
+        //gambar
+        URL poster_url = NetworkUtils.buildMoviePosterUrl(movie.getPosterPath());
+        Glide.with(this)
+                .load(poster_url)
+                .into(mMovieImage);
+
+        //year
+        mMovieYear.setText(movie.getReleaseDate());
+
+        //rating
+        mMovieRating.setText(String.valueOf(movie.getVoteAverage()));
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void save_movie(View v){
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(MovieContract.MovieList.COLUMN_TITLE, movie.getTitle());
+        contentValues.put(MovieContract.MovieList.COLUMN_PATH, movie.getPosterPath());
+        contentValues.put(MovieContract.MovieList.COLUMN_OVERVIEW, movie.getOverview());
+        contentValues.put(MovieContract.MovieList.COLUMN_RELEASE_DATE, movie.getReleaseDate());
+        contentValues.put(MovieContract.MovieList.COLUMN_VOTE_AVERAGE, movie.getVoteAverage());
+        // Insert the content values via a ContentResolver
+        Uri uri = getContentResolver().insert(MovieContract.MovieList.CONTENT_URI, contentValues);
+
+        if(uri != null) {
+            Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 }
--- app/src/main/java/com/example/android/iakintermedietpopularmovie/MainActivity.java
+++ app/src/main/java/com/example/android/iakintermedietpopularmovie/MainActivity.java
@@ -1,28 +1,124 @@
 package com.example.android.iakintermedietpopularmovie;
 
-import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
-import android.widget.GridView;
-import android.widget.TextView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import com.example.android.iakintermedietpopularmovie.adapter.MovieAdapter;
+import com.example.android.iakintermedietpopularmovie.model.Movie;
+import com.example.android.iakintermedietpopularmovie.utils.NetworkUtils;
+import com.example.android.iakintermedietpopularmovie.utils.RecyclerViewClickListener;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements RecyclerViewClickListener{
 
-    @BindView(R.id.rv_movie);
+    @BindView(R.id.rv_movie) RecyclerView mMovieView;
+    @BindView(R.id.pb_loading) ProgressBar mLoadingView;
+
+    private MovieAdapter adapter;
+    private List<Movie> movieList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        adapter = new MovieAdapter(this, movieList, this);
+
+        RecyclerView.LayoutManager layoutManager = new GridLayoutManager(this, 2);
+        mMovieView.setLayoutManager(layoutManager);
+        mMovieView.setAdapter(adapter);
+
+        loadData();
+    }
+
+    private void loadData(){
+        URL url = NetworkUtils.buildUrl("popular");
+        new GetDataTask().execute(url);
+    }
+
+    @Override
+    public void onItemClicked(int position) {
+        //Toast.makeText(this, "position"+position, Toast.LENGTH_SHORT).show();
+
+        Movie movie = movieList.get(position);
+
+        Intent intent = new Intent(this , DetailMovieActivity.class);
+        intent.putExtra(DetailMovieActivity.MOVIE_INTENT, movie);
+
+        startActivity(intent);
+    }
+
+    public class GetDataTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mMovieView.setVisibility(View.GONE);
+            mLoadingView.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            URL url = urls[0];
+            String result= null;
+
+            try {
+                result = NetworkUtils.getResponseFromHttpUrl(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String strings) {
+            super.onPostExecute(strings);
+            mLoadingView.setVisibility(View.GONE);
+            mMovieView.setVisibility(View.VISIBLE);
+
+            Log.e("json_object", strings);
+            try {
+                JSONObject jsonObject = new JSONObject(strings);
+                JSONArray results = jsonObject.getJSONArray("results");
+
+                for(int i=0; i<results.length(); i++){
+                    movieList.add(new Movie(
+                            results.getJSONObject(i).getInt("id"),
+                            results.getJSONObject(i).getString("title"),
+                            results.getJSONObject(i).getString("poster_path"),
+                            results.getJSONObject(i).getString("overview"),
+                            results.getJSONObject(i).getString("release_date"),
+                            results.getJSONObject(i).getDouble("vote_average"))
+                    );
+                }
+
+                adapter.notifyDataSetChanged();
+
+                Log.e("json_array", strings);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
